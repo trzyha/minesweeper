a0aefb49f8814d32a1fdd0143f777448b3906023
trzyha <39124933+trzyha@users.noreply.github.com>
2019-03-05 11:34:13 +0100

    Add files via upload

--- miner.py
+++ miner.py
@@ -3,13 +3,15 @@ import tkinter as tk
 from functools import partial
 from itertools import product
 
-fieldsX, fieldsY = 6, 3 ## determine gameboard x and y
+fieldsX, fieldsY = 9, 9 ## determine gameboard x and y
 buttonW, buttonH = 3,1 ## setting size of buttons
-lst = (["",]*fieldsX,)*fieldsY
+lst = [[0 for _ in range(fieldsX)] for _ in range(fieldsY)] ##creates dynamically 2d list
+#lst = ([""]*fieldsX,[""]*fieldsX,[""]*fieldsX) ## filling with n copies 
+
 positions = product(range(fieldsY), range(fieldsX))
 #mines = randint(1,9)
-i=0
-mines = 5
+
+mines = 10
 buttonIdentities = []
 #buttonIdentities = {} ##when naming buttons needs to be {}
 
@@ -18,40 +20,41 @@ def change(i):
 	buttonPosition = ([int(i/fieldsX),i%fieldsX]) #gets list position
 	buttPosX = int(i/fieldsX)
 	buttPosY = i%fieldsX
-	#print(buttPosX, buttPosY) ##testing position
-	#print(lst[buttPosX][buttPosY])
+	print(buttPosY,buttPosX)
 	bname.configure(text=lst[buttPosX][buttPosY], relief=tk.SUNKEN)
 	if lst[buttPosX][buttPosY] == "*":
 		print("BOOM you're dead")
-
-
-
-for i in range (1,mines+1)
+		"""for a in range(0,fieldsX*fieldsY):
+			bname=(buttonIdentities[a])
+			bname.configure(text=lst[buttPosX][buttPosY], relief=tk.SUNKEN)"""
+	else:
+		minesCount = 0
+		if lst[buttPosX-1][buttPosY] == "*":
+			minesCount =+ 1
+		elif lst[buttPosX+1][buttPosY] == "*":
+			minesCount =+ 1
+		elif lst[buttPosX-1][buttPosY-1] == "*":
+			minesCount =+ 1
+		elif lst[buttPosX][buttPosY-1] == "*":
+			minesCount =+ 1
+		elif lst[buttPosX+1][buttPosY-1] == "*":
+			minesCount =+ 1		
+		elif lst[buttPosX-1][buttPosY+1] == "*":
+			minesCount =+ 1
+		elif lst[buttPosX][buttPosY+1] == "*":
+			minesCount =+ 1
+		elif lst[buttPosX+1][buttPosY+1] == "*":
+			minesCount =+ 1
+		print (minesCount)
+		bname.configure(text=minesCount, relief=tk.SUNKEN)
+for i in range (0,mines):
 	randomX=randint(0,fieldsX-1)
 	randomY=randint(0,fieldsY-1)
-	#if lst[randomX-1][randomY-1] != "*":
-	lst[1][1] = "*"
-	print(randomX, randomY)
-	#lst[randomX-1][randomY-1] = "*"
-
-#print(len(lst))
-"""for y in range (0, fieldsY):
-	for x in range (0,fieldsX):
-		mineYesNo = randint(1,3)
-		if mineYesNo == 1:
-			lst[y][x] = "*"""
-			
-print(lst)
-
+	lst[randomY][randomX] = "*"
 
 root = tk.Tk()
 root.title("Minesweeper")
 root.minsize(width=fieldsX*buttonW, height=fieldsY*buttonH)
-"""for iY in range(fieldsY):
-	for iX in range(fieldsX):
-		btn = tk.Button(root, width=2, command=partial(change,iX,iY))
-		btn.grid(row=iY, column=iX)
-		buttonIdentities.append(btn)"""
 
 for i, item in enumerate(positions):
     button = tk.Button(root, command=partial(change, i),width=buttonW, height=buttonH)
